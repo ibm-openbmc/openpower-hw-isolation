266d79cde9d1987147b81a8d8ccb737a170b0bd3
Ramesh Iyyar <rameshi1@in.ibm.com>
2021-11-15 22:13:52 -0600

    record: manager: Added API to get record details
    - In this patch, added the API to get the isolated hardware record
      details (severity and error log) based on the given hardware
      inventory path.
    
    - The returned record details will help to create the respective
      hardware status to represent the hardware status.
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: Ib669c4986bc80107c7bd40c857c520ffafa5bb20

--- src/hw_isolation_record/manager.cpp
+++ src/hw_isolation_record/manager.cpp
@@ -482,5 +482,53 @@ sdbusplus::message::object_path Manager::createWithEntityPath(
     return *entryPath;
 }
 
+std::optional<std::tuple<entry::EntrySeverity, entry::EntryErrLogPath>>
+    Manager::getIsolatedHwRecordInfo(
+        const sdbusplus::message::object_path& hwInventoryPath)
+{
+    // Make sure whether the given hardware inventory is exists
+    // in the record list.
+    auto entryIt =
+        std::find_if(_isolatedHardwares.begin(), _isolatedHardwares.end(),
+                     [hwInventoryPath](const auto& ele) {
+                         bool isIsolated = false;
+                         for (const auto& assocEle : ele.second->associations())
+                         {
+                             if ((std::get<0>(assocEle) == "isolated_hw") &&
+                                 (std::get<2>(assocEle) == hwInventoryPath.str))
+                             {
+                                 isIsolated = true;
+                                 break;
+                             }
+                         }
+
+                         // Make sure whether the given hardware inventory is
+                         // not resolved because the same hardware might be
+                         // isolated and resolved many times.
+                         if (isIsolated)
+                         {
+                             return !ele.second->resolved();
+                         }
+                         return false;
+                     });
+
+    if (entryIt == _isolatedHardwares.end())
+    {
+        return std::nullopt;
+    }
+
+    entry::EntryErrLogPath errLogPath;
+    for (const auto& assocEle : entryIt->second->associations())
+    {
+        if (std::get<0>(assocEle) == "isolated_hw_errorlog")
+        {
+            errLogPath = std::get<2>(assocEle);
+            break;
+        }
+    }
+
+    return std::make_tuple(entryIt->second->severity(), errLogPath);
+}
+
 } // namespace record
 } // namespace hw_isolation
